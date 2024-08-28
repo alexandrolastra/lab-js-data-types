51d8bb7c9f0039a7680b30be5900156120960082
alexandrolastra <alexandrolastra.fr@gmail.com>
2024-08-29 00:34:24 +0200

    FT_RMT_082024 Alexandro & Kevin

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const s2 = "fed";
 const s3 = "Ted";
 const s4 = "bread";
 const s5 = "and";
-
+const s = " "
 // Concatenate the string variables into one new string
-
-
+const result11 = s1 +s+ s5  +s+ s3 +s+ s2 +s+ s4 
+const result111 =`${s1} ${s5} ${s3} ${s2} ${s4}` 
 // Print out the concatenated string
-
-
+console.log(result11)
+console.log(result111)
 
 
 /*******************************************
@@ -23,11 +23,11 @@ const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
 
+const cutPart1 = part1.slice(0,-1) + (part1.slice(-1).toUpperCase()); 
 
-// Print the cameLtaiL-formatted string
+const cutPart2 = part2.slice(0,-1) + (part2.slice(-1).toUpperCase()); 
 
-
-
+console.log(cutPart1 + cutPart2 )
 
 /*******************************************
     Iteration 2.1 | Calculate Tip
@@ -35,11 +35,11 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
-
+const tipPP = 0.15;
+const calculTip = billTotal*tipPP
 // Print out the tipAmount
-
-
+console.log("The tip is :" + calculTip +s+ "EUR")
+console.log( calculTip)
 
 
 /*******************************************
@@ -48,9 +48,9 @@ const billTotal = 84;
 
 // Generate a random integer between 1 and 10 (inclusive)
 
-
+const result22 = Math.round(Math.random()*10);
 // Print the generated random number
-
+console.log(result22)
 
 
 /*******************************************
@@ -62,15 +62,16 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
-
+'false' 
 const expression2 = a || b;
-
+'false' 
 const expression3 = !a && b;
-
+'true'
 const expression4 = !(a && b);
-
+'true'
 const expression5 = !a || !b;
-
+'true'
 const expression6 = !(a || b);
-
+'false' 
 const expression7 = a && a;
+'true'
